54ced0a81ba261ce1e49c40f75c513ea2d4a720a
maheralky <maherjood94@gmail.com>
2022-10-25 15:15:00 +0200

    Add 3 interfaces

---
+++ Ladeskab/Interfaces/IDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab.Interfaces
+{
+    public interface IDisplay
+    {
+        void ConnectPhone();
+        void Erorr_ConnectPhone();
+        void ReadRfid();
+        void Erorr_ReadRfid();
+        void PhoneCharging();
+        void Erorr_PhoneCharging();
+
+    }
+}
---
+++ Ladeskab/Interfaces/IDoor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab.Interfaces
+{
+    public interface IDoor
+    {
+        
+
+        
+        void LockDoor();
+        void UnlockDoor();
+
+        void DoorOpened();
+
+        void DoorClosed();
+
+    }
+}
