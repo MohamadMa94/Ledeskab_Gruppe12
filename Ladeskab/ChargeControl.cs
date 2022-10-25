3bb4faad9c9a0fb0f567c98564680c4847661c07
KasperMartensen <tigriss.tiger@gmail.com>
2022-10-25 15:28:13 +0200

    Update ChargeControl.cs

--- Ladeskab/ChargeControl.cs
+++ Ladeskab/ChargeControl.cs
@@ -4,11 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UsbSimulator;
 
 namespace ImpLadeskab
 {
     public class ChargeControl : IChargeControl
     {
-        
+        IUsbCharger _USBCharger;
+
+        public double ControlCurrent {get; private set; }
+
+        public ChargeControl()
+        {
+            _USBCharger = new UsbChargerSimulator();
+            _USBCharger.CurrentValueEvent += HandleCurrentValueChanged;
+        }
+
+        public void HandleCurrentValueChanged(object sender, CurrentEventArgs e)
+        {
+            ControlCurrent = e.Current;
+        }
+
+
     }
 }
