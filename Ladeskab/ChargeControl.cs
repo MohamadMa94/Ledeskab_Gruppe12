2d707c840e4660124305431e69801c72ab4cf534
KasperMartensen <tigriss.tiger@gmail.com>
2022-10-25 15:26:29 +0200

    Added Chargecontroller

---
+++ Ladeskab/ChargeControl.cs
@@ -0,0 +1,14 @@
+﻿using ImpLadeskab.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpLadeskab
+{
+    public class ChargeControl : IChargeControl
+    {
+        
+    }
+}
