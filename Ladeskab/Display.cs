1cdec12a8dfa75d2060d029229bda0d2dffcc188
maheralky <maherjood94@gmail.com>
2022-10-27 12:03:43 +0200

    ..

--- Ladeskab/Display.cs
+++ Ladeskab/Display.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,41 +11,5 @@ namespace Ladeskab
 {
     public class Display : IDisplay
     {
-
-        void IDisplay.ConnectPhone()
-        {
-            Console.WriteLine("Please connect your phone to the charging .\n");
-        }
-
-        void IDisplay.Erorr_ConnectPhone()
-        {
-            Console.WriteLine("ERROR! The phone is not connected.\n");
-        }
-
-        void IDisplay.Erorr_PhoneCharging()
-        {
-            Console.WriteLine("ERROR! Phone is not charging.\n");
-        }
-
-        void IDisplay.Erorr_ReadRfid()
-        {
-            Console.WriteLine("ERROR! RFID chip is not on the lock.\n");
-        }
-
-        void IDisplay.PhoneChargeDone()
-        {
-            Console.WriteLine("Phone is fully charged\n");
-        }
-
-        void IDisplay.PhoneCharging()
-        {
-            Console.WriteLine("Phone is currently charging.\n");
-        }
-
-        void IDisplay.ReadRfid()
-        {
-            Console.WriteLine("Place RFID chip on the lock.\n");
-        }
-
     }
 }
