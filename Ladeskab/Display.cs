e635f6473a62de6e04cea974645bd1ff3a13b724
maheralky <maherjood94@gmail.com>
2022-10-27 10:37:31 +0200

    Display implementeret

--- Ladeskab/Display.cs
+++ Ladeskab/Display.cs
@@ -9,5 +9,34 @@ namespace Ladeskab
 {
     public class Display : IDisplay
     {
+        void IDisplay.ConnectPhone()
+        {
+            Console.WriteLine("Please connect your phone to the charging .\n");
+        }
+
+        void IDisplay.Erorr_ConnectPhone()
+        {
+           Console.WriteLine("ERROR! The phone is not connected.\n");
+        }
+
+        void IDisplay.Erorr_PhoneCharging()
+        {
+            Console.WriteLine("ERROR! Phone is not charging.\n");
+        }
+
+        void IDisplay.Erorr_ReadRfid()
+        {
+            Console.WriteLine("ERROR! RFID chip is not on the lock.\n");
+        }
+
+        void IDisplay.PhoneCharging()
+        {
+            Console.WriteLine("Phone is currently charging.\n");
+        }
+
+        void IDisplay.ReadRfid()
+        {
+            Console.WriteLine("Place RFID chip on the lock.\n");
+        }
     }
 }
