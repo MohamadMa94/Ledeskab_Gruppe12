4ddedadf9c26ba9cc06a7ea2cc05dd83215ccb19
MaziatHassan <83606847+MaziatHassan@users.noreply.github.com>
2022-10-28 11:03:06 +0200

    TestLogFile

---
+++ Ladeskab.Test.Unit/TestLogFile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+
+namespace Ladeskab.Test.Unit
+{
+    public class TestLogFile
+    {
+       
+        private LogFile _uut;
+        private IWrite _write;
+
+        [SetUp]
+        public void Setup()
+        {
+           _write = Substitute.For<IWrite>();
+            _uut = new LogFile(_write);
+        }
+
+        [TestCase(1)]
+        public void LogDoorLocked_CallsWriteLineLocked(int id)
+        {
+            _uut.LogDoorLocked(id);
+            _write.Received(1).LockedMessage(id);
+        }
+
+        [TestCase(1)]
+        public void LogDoorUnlocked_CallsWriteLineUnlocked(int id)
+        {
+            _uut.LogDoorUnlocked(id);
+            _write.Received(1).UnlockedMessage(id);
+        }
+    }
+
+
+}
---
+++ Ladeskab.Test.Unit/TestWrite.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab.Interfaces;
+using NUnit.Framework;
+
+namespace Ladeskab.Test.Unit
+{
+    public class TestWrite
+    {
+        private Write _uut;
+        private StringWriter _output;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Write();
+
+            _output = new StringWriter();
+            System.Console.SetOut(_output);
+        }
+
+        [TestCase(50)]
+        public void LockedMessage_OutputString_IsEqualTo_Expected(int id)
+        {
+            string expected = DateTime.Now + ": Skab låst med RFID: " + id;
+
+            string result = _uut.LockedMessage(id);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(50)]
+        public void UnlockedMessage_OutputString_IsEqualTo_Expected(int id)
+        {
+            string expected = DateTime.Now + ": Skab låst op med RFID: " + id;
+
+            string result = _uut.UnlockedMessage(id);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+
+
+
+
+}
