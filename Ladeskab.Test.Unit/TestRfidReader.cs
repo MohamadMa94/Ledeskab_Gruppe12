1ea88cbcb244b82ed302b59cd6aeb4c62de69c85
MaziatHassan <83606847+MaziatHassan@users.noreply.github.com>
2022-10-28 21:12:15 +0200

    RfidReadertest

---
+++ Ladeskab.Test.Unit/TestRfidReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Ladeskab.Test.Unit
+{
+    internal class TestRfidReader
+    {
+    }
+}
